d756349defd57a920dcdcc4d355f9ed628fa6d0e
Petro Darii <petro.darii@gmail.com>
2017-12-06 23:17:36 +0200

    Moment locale

--- src/app/components/addclient/addclient.component.ts
+++ src/app/components/addclient/addclient.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
+import 'moment/locale/ru';
+import 'moment/locale/uk';
+
 import { ClientsService } from './../../services/clients.service';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-addclient',
@@ -9,14 +13,21 @@ import { ClientsService } from './../../services/clients.service';
 })
 export class AddclientComponent implements OnInit {
 
-  constructor(private clientsService: ClientsService) { }
+  locale = 'uk';
+  bsConfig: Partial<BsDatepickerConfig>;
+
+  constructor(private clientsService: ClientsService) {
+    moment.locale(this.locale);
+  }
 
   ngOnInit() {
+    console.log(moment.locales());
+    this.bsConfig = Object.assign({}, { locale: this.locale });
+
   }
 
   public addClient() {
-    
-
+    const data = ' ';
     this.clientsService.addClient(data).subscribe((id) => {
       console.log(`Client added ${id}`);
     });
