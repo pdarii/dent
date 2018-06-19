df896478d643316d1dc0d97b32dae457d32e0bbf
Petro Darii <petro.darii@gmail.com>
2018-06-19 15:36:25 +0300

    Dist

--- server/app.js
+++ server/app.js
@@ -1,110 +1,129 @@
-const express = require('express')
-const DBService = require('./services/dbservice')
-const path = require('path')
+//Install express server
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('DB service listening on port 3000!')
-})
-
-app.post('/api/deleteClient',function(req,res){
-  const clientsServiceObj = new DBService(req, res)
-  clientsServiceObj.deleteClient(req.body.id);
-});
-
-app.post('/api/addClient', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  const client = req.params;
-  clientsServiceObj.addClient(req.body);
-})
-
-app.post('/api/planClient', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  const client = req.params;
-  clientsServiceObj.planClient(req.body);
-})
-
-app.post('/api/saveClient', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  const client = req.params;
-  clientsServiceObj.saveClient(req.body);
-})
-
-app.get('/api/getClients', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  clientsServiceObj.getClients();
-})
-
-app.get('/api/getDoctors', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  clientsServiceObj.getDoctors();
-})
-
-app.get('/api/getBirthdaysCount', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  clientsServiceObj.getBirthdaysCount();
-})
-
-app.get('/api/getCalendarData', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  clientsServiceObj.getCalendarData();
-})
-
-app.get('/api/getClientsCount', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  clientsServiceObj.getClientsCount();
-})
-
-app.get('/api/getClientById/:id', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  const id = req.params.id;
-  clientsServiceObj.getClientById(id);
-})
-
-app.get('/api/searchClient/:name', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  const name = req.params.name;
-  clientsServiceObj.searchClient(name);
-})
-
-app.post('/api/getStatistic/', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  clientsServiceObj.getStatistic(req.body);
-})
-
-app.get('/api/getJobs', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  clientsServiceObj.getJobs();
-})
-
-app.get('/api/getTimelineEvents/:id', function (req, res) {
-  const clientsServiceObj = new DBService(req, res)
-  const id = req.params.id;
-  clientsServiceObj.getTimelineEvents(id);
-})
-
-
-// APP REQUESTS
-// app.get('/', (req, res) =>  res.sendFile(path.resolve('dist/index.html')));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve('dist/index.html'));
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 
 
 
-
-
-
-
-
+// const express = require('express')
+// const DBService = require('./services/dbservice')
+// const path = require('path')
+//
+// const app = express()
+//
+// const bodyParser = require('body-parser')
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+//
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+//
+// app.listen(3000, function () {
+//   console.log('DB service listening on port 3000!')
+// })
+//
+// app.post('/api/deleteClient',function(req,res){
+//   const clientsServiceObj = new DBService(req, res)
+//   clientsServiceObj.deleteClient(req.body.id);
+// });
+//
+// app.post('/api/addClient', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   const client = req.params;
+//   clientsServiceObj.addClient(req.body);
+// })
+//
+// app.post('/api/planClient', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   const client = req.params;
+//   clientsServiceObj.planClient(req.body);
+// })
+//
+// app.post('/api/saveClient', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   const client = req.params;
+//   clientsServiceObj.saveClient(req.body);
+// })
+//
+// app.get('/api/getClients', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   clientsServiceObj.getClients();
+// })
+//
+// app.get('/api/getDoctors', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   clientsServiceObj.getDoctors();
+// })
+//
+// app.get('/api/getBirthdaysCount', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   clientsServiceObj.getBirthdaysCount();
+// })
+//
+// app.get('/api/getCalendarData', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   clientsServiceObj.getCalendarData();
+// })
+//
+// app.get('/api/getClientsCount', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   clientsServiceObj.getClientsCount();
+// })
+//
+// app.get('/api/getClientById/:id', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   const id = req.params.id;
+//   clientsServiceObj.getClientById(id);
+// })
+//
+// app.get('/api/searchClient/:name', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   const name = req.params.name;
+//   clientsServiceObj.searchClient(name);
+// })
+//
+// app.post('/api/getStatistic/', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   clientsServiceObj.getStatistic(req.body);
+// })
+//
+// app.get('/api/getJobs', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   clientsServiceObj.getJobs();
+// })
+//
+// app.get('/api/getTimelineEvents/:id', function (req, res) {
+//   const clientsServiceObj = new DBService(req, res)
+//   const id = req.params.id;
+//   clientsServiceObj.getTimelineEvents(id);
+// })
+//
+//
+// // APP REQUESTS
+// // app.get('/', (req, res) =>  res.sendFile(path.resolve('dist/index.html')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve('dist/index.html'));
+// });
+//
+//
+//
+//
+//
+//
+//
+//
