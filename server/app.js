bccc865e04a6959406707be81757de00ea7255d3
Petro Darii <petro.darii@gmail.com>
2018-06-21 22:29:31 +0300

    Dist

--- server/app.js
+++ server/app.js
@@ -98,7 +98,7 @@ app.get('/*', function(req,res) {
   console.log('++++++++++++');
   console.log(req.originalUrl);
   console.log('++++++++++++');
-  res.sendFile(path.join(__dirname+'./../dist/'));
+  res.sendFile(path.join(__dirname+'./../dist'));
 });
 
 
